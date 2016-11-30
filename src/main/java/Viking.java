9641587af9ab9d9fada13df6af14e8fe40d5f7c2
SBT-Galimov-RR <romangalimov@gmail.com>
2016-11-30 14:15:13 +0400

    коменты

--- src/main/java/Viking.java
+++ src/main/java/Viking.java
@@ -5,24 +5,24 @@ package main.java;
  */
 public class Viking extends Warrior implements Cloneable {
 
-    private final int DAMAGE = 55;
+    private final int DAMAGE = 55; //эти переменные нигде не используются
     private final long HEALTH = 200;
 
 
     public Viking(){
         damage = 55;
-        health = 200;
+        health = 20;
     }
 
     public Viking(String warriorName, String squadName){
         damage = 55;
         health = 200;
-        this.warriorName = warriorName;
+        this.warriorName = warriorName; //копипаста из 3-го конструктора
         this.squadName = squadName;
     }
 
     public Viking(String warriorName){
-        damage = 55;
+        damage = 55; //копипаста из конструктора по умолчанию
         health = 200;
         this.warriorName = warriorName;
     }
--- src/main/java/Warrior.java
+++ src/main/java/Warrior.java
@@ -3,7 +3,7 @@ package main.java;
 /**
  * Created by Vo4ik on 26.11.2016.
  */
-public class Warrior implements WarriorInter, Cloneable {
+public class Warrior implements WarriorInter, Cloneable { //если этот класс не предназначен для создания объектов, его следует сделать абстрактным
 
     protected int damage;
     protected long health;
@@ -49,7 +49,7 @@ public class Warrior implements WarriorInter, Cloneable {
 
     @Override
     public boolean isAlive() {
-        if(health > 0){
+        if(health > 0){ //в местах, где проверяется/возвращается результат условия, можно сразу проверять/возвращать. в данном случае просто return health > 0;
             return true;
         }else{
             return false;
@@ -64,7 +64,7 @@ public class Warrior implements WarriorInter, Cloneable {
     @Override
     public String toString(){
         //Вывод имя, клас и пренадлежность к отряду
-        StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder(); //это избыточное создание кучи объектов. Либо использовать 1 format, либо просто конкатенацию строк
         sb.append(String.format("Имя: %s\n", this.warriorName));
         sb.append(String.format("Класс: %s\n", this.getClass().getSimpleName()));
         sb.append(String.format("Отряд: %s\n", this.squadName));
@@ -77,10 +77,10 @@ public class Warrior implements WarriorInter, Cloneable {
             Warrior w = (Warrior) super.clone();
 
             return w;
-        }catch (Exception e){return null;}
-    }
+        }catch (Exception e){return null;} //тут теряется вся информация о произошедшем исключении и возвращается null туда, где ожидается Warrior, что в будущем вызовет null pointer exception.
+    }//+ плохое форматирование
 
     public String getInfo(){
-        return ("Дамаг: " + this.damage + "\nХП: " + this.health);
+        return ("Дамаг: " + this.damage + "\nХП: " + this.health); //а это не должно быть частью toString?
     }
 }
